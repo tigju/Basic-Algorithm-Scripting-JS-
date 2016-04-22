3d3993b0efb8e64c4d90b4a78e072cd219aa2623
unknown <tigju24@gmail.com>
2016-04-21 20:26:34 -0400

    seek and destroy modified

--- Seek and Destroy/seek_and_destroy.js
+++ Seek and Destroy/seek_and_destroy.js
@@ -1,6 +1,6 @@
 function destroyer(arr) {
   // Remove all the values
-  var args = Array.prototype.slice.call(arguments, 1);
+  var args = [].slice.call(arguments, 1); // or var args = Array.prototype.slice.call(arguments, 1);
   function remove(num) {
     return args.indexOf(num) === -1;
     }
