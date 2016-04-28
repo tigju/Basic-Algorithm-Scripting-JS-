c9b31f7e835d881350d52cee4d774e3f3ea34208
unknown <tigju24@gmail.com>
2016-04-27 23:20:02 -0400

    Caesars Cipher

--- Caesars Cipher/Caesars_Cipher.js
+++ Caesars Cipher/Caesars_Cipher.js
@@ -3,20 +3,20 @@ function rot13(str) { // LBH QVQ VG!
   var start = 'A'.charCodeAt();
   var end = 'Z'.charCodeAt();
   var charStr ='';
-  var encodedStr ='';
+  var decodedStr ='';
   for (var i = 0; i<str.length; i++) {
   	charStr = str[i].charCodeAt();
   	if(charStr < start) {
-  		encodedStr += String.fromCharCode(charStr);
+  		decodedStr += String.fromCharCode(charStr);
   	} else if(charStr + key <= end) {
    	    charStr += key;
-   		encodedStr += String.fromCharCode(charStr);
+   		decodedStr += String.fromCharCode(charStr);
    } else {
    	    charStr = (charStr + key)%26+(start - key);
-   	    encodedStr += String.fromCharCode(charStr);
+   	    decodedStr += String.fromCharCode(charStr);
    }
   }
-  return encodedStr;
+  return decodedStr;
 }
 
 // Change the inputs below to test
